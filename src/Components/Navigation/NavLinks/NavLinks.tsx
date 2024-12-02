4fced9146760b9dda53095141b19d1bb13f5bd95
NinaNorby <norbyy_@hotmail.com>
2024-12-02 13:45:53 +0100

    20241202 CSS Navlink   transform: translateY

--- src/Components/Navigation/NavLinks/NavLinks.tsx
+++ src/Components/Navigation/NavLinks/NavLinks.tsx
@@ -1,18 +1,18 @@
 //  NavLinks komponenten innehåller två länkar: "Home" och "Shopping cart".
 import { Link } from "react-router-dom";
-import styles from './NavLinks.module.css';
+import styles from "./NavLinks.module.css";
 
-// destrukturering av props. med onclick . onclick är valfritt iom ? så att det är valfritt att klicka på länekn ,  samt att jag har satt typen  till void.oncklick är en funktion som inte tar några argument. 
+// destrukturering av props. med onclick . onclick är valfritt iom ? så att det är valfritt att klicka på länekn ,  samt att jag har satt typen  till void.oncklick är en funktion som inte tar några argument.
 const NavLinks = ({ onClick }: { onClick?: () => void }) => (
   <ul className={styles.navMenu}>
-    <li className={styles.navItem}> 
+    <li className={styles.navItem}>
       <Link to="/" onClick={onClick} className={styles.navLink}>
-      <i className="fa-solid fa-house"></i> Home 
+        <i className="fa-solid fa-house"></i> Home
       </Link>
     </li>
     <li className={styles.navItem}>
       <Link to="/cart" onClick={onClick} className={styles.navLink}>
-      <i className="fa-solid fa-cart-shopping"></i> Shopping cart 
+        <i className="fa-solid fa-cart-shopping"></i> Shopping cart
       </Link>
     </li>
   </ul>
