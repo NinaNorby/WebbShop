71b50349c4f60c58b18a0dc8fc3054e5ae599048
NinaNorby <norbyy_@hotmail.com>
2024-12-03 09:57:47 +0100

    version 20241203 uppd. för gtest app.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,11 @@
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
-
 import App from "./App";
 
+
+//TEST FÖR HEADING 
 describe("App component", () => {
-  it("should have a correct heading", () => {
+  it("should render the correct heading", () => {
     render(<App />);
 
     const heading = screen.getByRole("heading", {
@@ -12,4 +13,33 @@ describe("App component", () => {
     });
     expect(heading).toBeInTheDocument();
   });
+
+  //TEST FÖR NAVLINK
+  it("should render the navigation bar with links", () => {
+    render(<App />);
+
+    // Kontrollera att navigationslänkarna finns
+    const homeLink = screen.getByRole("link", { name: /home/i });
+    const cartLink = screen.getByRole("link", { name: /shopping cart/i });
+
+    expect(homeLink).toBeInTheDocument();
+    expect(cartLink).toBeInTheDocument();
+  });
+
+
+  //TETST FÖR FOOTER
+  it("should render the footer with a coptright sybole and the current year", () => {
+    render(<App />);
+
+    const footer = screen.getByText((content, element) => {
+      return (
+        content.includes("©") &&
+        content.includes("2024") &&
+        element?.tagName.toLowerCase() === "p"
+      );
+    });
+    expect(footer).toBeInTheDocument();
+  });
+  
+  
 });
